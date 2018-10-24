3a9f690d2a0050974a65f075063d61653a01b66a
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-10-24 13:53:48 +0100

    chore: refactor landing frame into own 'Hero' component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,24 @@
 <template>
-	<div class="Home inner">
-		<pre class="Home__title">Michael Fitzhavey</pre>
+	<section class="Home">
 
-		<img
-			class="Home__hero"
-			src="../assets/logo.svg"
-			alt="code image">
+		<hero/>
 
-		<section class="text-align-left">
-			<p>Full Stack developer based in London. Specialising in Node.js, Firebase & Vue.</p>
-			<p>I use agile development methods and cutting edge web technonology to deliver rapid results.</p>
-		</section>
-	</div>
+	</section>
 </template>
+
+<script>
+import Hero from '@/components/Hero.vue';
+
+export default {
+	components: {
+		Hero,
+	},
+};
+</script>
+
 
 <style lang="scss">
 
-@import '../styles/index.scss';
-
-.Home {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	min-height: 100vh;
-
-	@media all and (min-width: $desktop) {
-		$headerHeight: 80px;
-		margin-top: $headerHeight;
-		min-height: calc(100vh - #{$headerHeight});
-	}
-
-	&__title {
-		font-size: 2rem;
-
-		@media all and (min-width: $tablet) {
-			font-size: 3rem;
-		}
-
-		@media all and (min-width: $desktop) {
-			font-size: 4rem;
-		}
-	}
-
-	&__hero {
-		width: 80%;
-		margin: 1rem 10%;
-
-		@media all and (min-width: $tablet) {
-			width: 70%;
-			margin: 2rem 15%;
-		}
-
-		@media all and (min-width: $desktop) {
-			width: 50%;
-			margin: 2rem 25%;
-		}
-	}
-
-}
 
 </style>
 
