1b2f60fb96b38448a5a7bbe58d162f256f7c619c
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2018-10-26 16:25:13 +0100

    release  (#13)
    * feat: update copy text
    
    * chore: remove unused HelloWorld.vue component
    
    * feat: readme improvements
    
    * feat: better layout for home screen
    
    * feat: ensure desktop and tablet support
    
    * feat: add github link to header
    
    * feat: add firebase hosting and deploy script
    
    * fix: reduced desktop breakpoint to 1024px
    
    * feat: position main homepage below header on desktop
    
    * fix: position key elements at top of screen
    
    * fix: forgot to add interpolation
    
    * Update README.md
    
    * chore: refactor landing frame into own 'Hero' component
    
    * chore: ensure comma dangle is disallowed
    
    * chore: add scaffold for WorkExperience component
    
    * chore: restyle header
    
    * feat: header now shows shadow on scroll
    
    * fix: sets hero height to auto when on screens with very short heights (wearables / mobile landscape)
    
    * feat: account for header in mobile vue and move max height on image
    
    * chore: comment out not yet ready section
    
    * Feat: add about me section (#9)
    
    * feat: install bootstrap-vue
    
    * feat: refactor styles to be easier to navigate
    
    * chore: rename WorkExperience to AboutMe to facilitate adding new component
    
    * feat: improve header styles
    
    * feat: improve helper classes
    
    * feat: use more bootstrap styling
    
    * chore: add image and format
    
    * feat: add draft about me content
    
    * feat: add margin bottom to the about me section
    
    * feat: add vue-analytics (#10)
    
    * Fix: styles (#11)
    
    * feat: make text-decoration on links a different colour
    
    * fix: hovering a link now only adds underline rather than also change colour
    
    * fix: remove underline from header title
    
    * feat: add circleCI config (#12)
    
    * Fix/circle ci config (#14)
    
    * feat: add circleCI config
    
    * fix: ensure a job named 'build' is present
    
    * fix: battle with circleCI
    
    * fix: keep battling circleCI

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # website-michael
 
+## Overview
+[This is a website](https://website-michael-fc46b.firebaseapp.com/) to demonstrate my development skills.
+
+### Requirements
+- mobile friendly (code follows a mobile first approach)
+- supports chrome, edge, safari, firefox, i.e. and opera
+
 ## Project setup
 ```
 npm install
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="AboutMe">
+		<b-container class="py-3">
+			<h1>About Me</h1>
+
+			<b-row class="pt-3">
+
+				<b-col class="responsive-col">
+					<img
+						class="AboutMe__image"
+						src="../assets/images/profile.jpg"
+						alt="Michael Fitzhavey"/>
+				</b-col>
+
+				<b-col class="responsive-col">
+					<p>
+						I'm a Node.js developer working at Oneflow Systems in London who specialises in rapidly building out front-end user interfaces. I am also proficient in back-end technologies and can build robust REST APIs (typically using the swagger framework).
+					</p>
+				</b-col>
+
+				<b-col class="responsive-col">
+					<p>I have experience with:</p>
+					<ul>
+						<li>HTML5, JS, CSS3</li>
+						<li>Typescript and CSS Compilers</li>
+						<li>VueJS, React and Angular</li>
+						<li>C#, .NET Core and Visual Basic</li>
+						<li>SQL and MongoDB</li>
+						<li>Swagger and Firebase</li>
+						<li>Mailchimp and CMS systems</li>
+					</ul>
+				</b-col>
+
+			</b-row>
+
+	</b-container>
+	</div>
+</template>
+
+<style lang="scss">
+
+@import '../styles/index.scss';
+
+.AboutMe {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background: $greyLight;
+	color: $grey;
+	text-align: left;
+
+	&__image {
+		width: 100%;
+	}
+
+}
+
+</style>
+
